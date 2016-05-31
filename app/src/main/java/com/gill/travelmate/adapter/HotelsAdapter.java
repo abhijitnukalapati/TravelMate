238a22777c3ef10558ded4ae3e5a9f49289e3a7b
Praneeth Vinjamuri <praneethvin.09@gmail.com>
2016-05-31 12:57:50 -0500

    feat: recycleview adapters for hotels, restaurants, places nearby and weather.

---
+++ app/src/main/java/com/gill/travelmate/adapter/HotelsAdapter.java
@@ -0,0 +1,90 @@
+package com.gill.travelmate.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.gill.travelmate.DetailActivity;
+import com.gill.travelmate.R;
+import com.gill.travelmate.arraylists.HotelsArraylist;
+import com.gill.travelmate.utils.GeneralValues;
+import com.gill.travelmate.utils.Utils;
+
+import java.util.ArrayList;
+
+//Adapter to add hotels list into HotelFragment
+public class HotelsAdapter extends RecyclerView.Adapter<HotelsAdapter.ChatHolder>{
+
+    private Context mContext;
+    private ArrayList<HotelsArraylist> list;
+    Intent i;
+
+    public HotelsAdapter(Context mContext, ArrayList<HotelsArraylist> list) {
+        this.mContext = mContext;
+        this.list = list;
+    }
+
+    @Override
+    public ChatHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        view= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_results, parent, false);
+
+        final ChatHolder holder = new ChatHolder(view);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i=new Intent(mContext, DetailActivity.class);
+                i.putExtra("check", GeneralValues.HOTEL_CHECK);
+                i.putExtra("position",""+holder.getAdapterPosition());
+                mContext.startActivity(i);
+                ((Activity)mContext).overridePendingTransition(R.anim.to_leftin, R.anim.to_leftout);
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ChatHolder holder, int position) {
+        holder.tv_name.setText(list.get(position).getName());
+        String reviews = list.get(position).getReviews()+ " "+mContext.getString(R.string.reviews_save);
+        holder.tv_reviews.setText(reviews);
+        holder.tv_categories.setText(list.get(position).getCategory());
+        holder.rating.setRating(Float.parseFloat(list.get(position).getRating()));
+        Glide.with(mContext).load(list.get(position).getImage()).placeholder(R.drawable.logo).error(R.drawable.no_image).into(holder.img_main);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ChatHolder extends RecyclerView.ViewHolder{
+
+        TextView tv_name,tv_reviews,tv_categories;
+        ImageView img_main,img_rating;
+        RatingBar rating;
+
+        public ChatHolder(View itemView) {
+            super(itemView);
+            tv_name = (TextView) itemView.findViewById(R.id.tv_name);
+            tv_reviews = (TextView) itemView.findViewById(R.id.tv_distance);
+            img_main = (ImageView) itemView.findViewById(R.id.img_main);
+            img_rating = (ImageView) itemView.findViewById(R.id.img_rating);
+            rating = (RatingBar) itemView.findViewById(R.id.rating);
+            tv_categories = (TextView) itemView.findViewById(R.id.tv_categories);
+
+            Utils.typeface_font(mContext,tv_name);
+            Utils.typeface_font(mContext,tv_reviews);
+            Utils.typeface_font(mContext,tv_categories);
+        }
+    }
+}
---
+++ app/src/main/java/com/gill/travelmate/adapter/WeatherAdapter.java
@@ -0,0 +1,137 @@
+package com.gill.travelmate.adapter;
+
+import android.content.Context;
+import android.os.Handler;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.gill.travelmate.R;
+import com.gill.travelmate.arraylists.WeatherArraylist;
+import com.gill.travelmate.fragments.WeatherFragment;
+import com.gill.travelmate.utils.TinyDB;
+import com.gill.travelmate.utils.Utils;
+
+import java.util.ArrayList;
+
+//Adapter to add data into WeatherFragment
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ChatHolder> {
+
+    private Context mContext;
+    private ArrayList<WeatherArraylist> list;
+    WeatherFragment fragment;
+    int item_position;
+    TinyDB tinyDB;
+    String check="";
+    int date_pos=0;
+
+    public WeatherAdapter(Context mContext, ArrayList<WeatherArraylist> list, WeatherFragment weatherFragment, int item_position, String check) {
+        this.mContext = mContext;
+        this.list = list;
+        fragment = weatherFragment;
+        this.item_position = item_position;
+        tinyDB=new TinyDB(mContext);
+        this.check=check;
+        if(item_position>5){
+            date_pos=6;
+        }else{
+            date_pos=5;
+        }
+    }
+
+    @Override
+    public ChatHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_weather, parent, false);
+
+        final ChatHolder holder = new ChatHolder(view);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().post(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            String date=list.get(holder.getAdapterPosition()).getDate();
+                            ArrayList<WeatherArraylist> arr=new ArrayList<WeatherArraylist>();
+                            arr=Utils.getWeatherArr(tinyDB);
+                            for(int i=0;i<arr.size();i++){
+                                if(date.equalsIgnoreCase(arr.get(i).getDate())){
+                                    fragment.setValues(i,check);
+                                    break;
+                                }
+                            }
+                        }catch (Exception e){
+
+                        }
+                    }
+                });
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ChatHolder holder, int position) {
+        holder.ll_outer.setVisibility(View.VISIBLE);
+        try{
+            if(position>date_pos){
+                String date = Utils.getDayFromTimeStamp(list.get(position).getDate())+"\n"+Utils.getDateFromTimeStamp(list.get(position).getDate());
+                holder.tv_day.setText(date);
+            }else{
+                holder.tv_day.setText(Utils.getDayFromTimeStamp(list.get(position).getDate()));
+            }
+        }catch (Exception ex){
+
+        }
+        String des="";
+        if(list.get(position).getW_full_des()!=null&&!list.get(position).getW_full_des().equalsIgnoreCase("")){
+            des = list.get(position).getW_full_des().substring(0,1).toUpperCase() + list.get(position).getW_full_des().substring(1);
+        }
+        holder.tv_weather.setText(des);
+
+        if(check.equalsIgnoreCase("C")){
+            //String celsius = "℃";
+            holder.tv_max_temp.setText(Utils.convert_K2C(list.get(position).getMax_temp()));
+            holder.tv_min_temp.setText(Utils.convert_K2C(list.get(position).getMin_temp()));
+        }else{
+            holder.tv_max_temp.setText( Utils.convert_K2F(list.get(position).getMax_temp()));
+            holder.tv_min_temp.setText( Utils.convert_K2F(list.get(position).getMin_temp()));
+        }
+        String img = "http://openweathermap.org/img/w/" + list.get(position).getW_icon() + ".png";
+        Glide.with(mContext).load(img).placeholder(R.drawable.logo).into(holder.img_weather);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ChatHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_day, tv_weather, tv_max_temp, tv_min_temp;
+        ImageView img_weather;
+        LinearLayout ll_outer;
+
+        public ChatHolder(View itemView) {
+            super(itemView);
+            tv_day = (TextView) itemView.findViewById(R.id.tv_day);
+            tv_weather = (TextView) itemView.findViewById(R.id.tv_weather);
+            tv_max_temp = (TextView) itemView.findViewById(R.id.tv_max_temp);
+            tv_min_temp = (TextView) itemView.findViewById(R.id.tv_min_temp);
+            img_weather = (ImageView) itemView.findViewById(R.id.img_weather);
+            ll_outer = (LinearLayout) itemView.findViewById(R.id.ll_outer);
+
+            Utils.typeface_font(mContext, tv_day);
+            Utils.typeface_font(mContext, tv_weather);
+            Utils.typeface_font(mContext, tv_max_temp);
+            Utils.typeface_font(mContext, tv_min_temp);
+        }
+    }
+}
