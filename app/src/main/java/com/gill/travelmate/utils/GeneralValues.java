1441b23ffb1d30ac51c70e438218e522897dd9b4
Praneeth Vinjamuri <praneethvin.09@gmail.com>
2016-05-31 13:05:40 -0500

    feat: insert your api keys for yelp and openweathermap here

---
+++ app/src/main/java/com/gill/travelmate/utils/GeneralValues.java
@@ -0,0 +1,55 @@
+package com.gill.travelmate.utils;
+
+
+//Here save all general values that are constant
+public class GeneralValues {
+
+    public static String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    public static String BASE_URL="http://winpraneeth.com/TravelMate/index.php/webservice/";
+    public static String FACEBOOK_LOGIN="facebookLogin?";
+    public static String USER_SIGNUP="registration?";
+    public static String USER_SIGNIN="login?";
+
+    public static String USER_ID_KEY="user_id";
+    public static String USER_NAME_KEY="user_name";
+    public static String USER_EMAIL_KEY="user_email";
+    public static String USER_DESTINATION_KEY="user_destination";
+    public static String USER_LAT_KEY="user_lat";
+    public static String USER_LONG_KEY="user_long";
+
+    public static String HOTEL_ARR="hotel_arr";
+    public static String RESTAURANT_ARR="restaurant_arr";
+    public static String PLACES_NEAR_ARR="places_near_arr";
+    public static String MY_SAVES_ARR="my_saves_arr";
+    public static String WEATHER_ARR="weather_arr";
+
+    // Insert your api keys here
+    public static String YELP_CUSTOMER_KEY="";
+    public static String YELP_CUSTOMER_SECRET="";
+    public static String YELP_TOKEN="";
+    public static String YELP_TOKEN_SECRET="";
+
+    public static String WEATHERMAP_KEY="";
+
+    public static String GOOGLE_SERVER_KEY="AIzaSyAM_WTIKVoVp2-DGddlNDPkhkZT6WoMC1Y";
+    public static String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place/autocomplete/json";
+
+    public static String WEATHER_BASE_URL="http://api.openweathermap.org/data/2.5/forecast/";
+    public static String WEATHER_DAILY_URL="daily?";
+
+    public static String HOTEL_FRAGMENT="hotel_fragment";
+    public static String RESTAURANT_FRAGMENT="reataurant_fragment";
+    public static String PLACES_FRAGMENT="places_fragment";
+
+    /*public static String HOTEL_SORT="hotel_sort";
+    public static String RESTAURANT_SORT="restaurant_sort";
+    public static String PLACES_SORT="places_sort";*/
+
+    public static String RESTAURANT_FILTER="restaurant_filter";
+    public static String PLACES_FILTER="places_filter";
+
+    public static String HOTEL_CHECK="hotel_check";
+    public static String RESTAURANT_CHECK="restaurant_check";
+    public static String PLACES_CHECK="places_check";
+    public static String MYSAVES_CHECK="mysaves_check";
+}
